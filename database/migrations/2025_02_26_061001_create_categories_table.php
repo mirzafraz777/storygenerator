7081d231b170d84b7aa27b50cb5a447d72c117c0
Faraz <mirzafraz777@gmail.com>
2025-08-05 16:35:07 +0500

    Bug Fixed

--- database/migrations/2025_02_26_061001_create_categories_table.php
+++ database/migrations/2025_02_26_061001_create_categories_table.php
@@ -24,12 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-<<<<<<< HEAD:database/migrations/2025_02_16_181018_alter_table_storygenerator_add_column_category.php
-        Schema::table('story_generators', function (Blueprint $table) {
-            $table->dropColumn('category');
-        });
-=======
         Schema::dropIfExists('categories');
->>>>>>> 71e712cf985e1cfceb8094c4c9b37d657a3a9918:database/migrations/2025_02_26_061001_create_categories_table.php
     }
 };
